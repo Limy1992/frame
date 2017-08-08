d601bc1bd0b5f9a2ce7e4a90f58bcc70bb544a4f
Limy1992 <limy_1992@163.com>
2017-08-08 17:25:14 +0800

    添加知乎图片选着器jar包,更换Glide图片加载器网络库

---
+++ app/src/main/java/com/lmy/audio/StartLogoActivity.java
@@ -0,0 +1,36 @@
+package com.lmy.audio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import lmy.com.utilslib.utils.LogUtils;
+
+/**
+ * Created by lmy on 2017/8/8
+ */
+
+public class StartLogoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_logo);
+
+        Observable.timer(3000, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Intent intent = new Intent(StartLogoActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        LogUtils.e("aLong="+aLong);
+                    }
+                });
+
+    }
+}
